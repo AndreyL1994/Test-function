5fa013357d9a1e64f362f47660a75fc9fca294d3
Andrey <102652716+AndreyL1994@users.noreply.github.com>
2022-04-01 12:14:43 +0300

    Update test_city_description_function.py

--- test_city_description_function.py
+++ test_city_description_function.py
@@ -4,11 +4,14 @@ from city_description_function import get_population_city_country
 
 
 class NameTestCase(unittest.TestCase):
+    """Тестирует функцию по заданным значениям"""
     def test(self):
+        """Тестирует со значениями город и страна"""
         message = get_population_city_country('penza', 'russia')
         self.assertEqual(message, 'Penza Russia')
 
     def test_all_values(self):
+        """Тестирует со значениями горд, страна и население"""
         message = get_population_city_country(
             'pattaya', 'thailand', 'population=2000000')
         self.assertEqual(message, 'Pattaya Thailand Population=2000000')
